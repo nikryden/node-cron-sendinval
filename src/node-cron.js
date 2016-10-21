df203d78daeb97a59f38a466a670ab20224a492c
niklas <nikryden@gmail.com>
2016-10-21 23:15:33 +0200

    first commit

---
+++ src/node-cron.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Task = require('./task'),
+  ScheduledTask = require('./scheduled-task');
+
+module.exports = (function() {
+
+  /**
+   * Creates a new task to execute given function when the cron
+   *  expression ticks.
+   *
+   * @param {string} expression - cron expression.
+   * @param {Function} func - task to be executed.
+   * @param {boolean} immediateStart - whether to start the task immediately.
+   * @returns {ScheduledTask} update function.
+   */
+  function createTask(expression, func, immediateStart,funcData) {
+    var task = new Task(expression, func,funcData);
+    return new ScheduledTask(task, immediateStart);
+  }
+
+  return {
+    schedule: createTask
+  };
+}());
